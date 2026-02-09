cf3cb259a35709177db6f861bb8f4b77288d7e6b
Jesushs4 <jherrerasanchez04@gmail.com>
2026-02-10 00:49:25 +0100

    project init with tailwind

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'InnerWork',
+  webDir: 'www'
+};
+
+export default config;
