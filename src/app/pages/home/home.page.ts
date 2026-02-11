05e14957e97a984468192343e0d6acae34e7265c
AntonioDR01 <antoniodr19903@gmail.com>
2026-02-11 17:39:56 +0100

    Carpeta cambiada de nombre y añadido las importaciones del componente

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { EmployeeAlertCardComponent } from 'src/app/shared/components/employee-alert-card/employee-alert-card.component';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, EmployeeAlertCardComponent],
 })
 export class HomePage {
   constructor() {}
