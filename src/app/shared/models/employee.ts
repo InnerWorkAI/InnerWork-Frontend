e1245117182f68bc80669ac91f1f0de5ce0a188f
AntonioDR01 <antoniodr19903@gmail.com>
2026-02-21 16:43:53 +0100

    Guardando cambios antes del merge

---
+++ src/app/shared/models/employee.ts
@@ -0,0 +1,20 @@
+export interface Employee {
+    id?: number;
+    first_name: string;
+    last_name: string;
+    birth_date: string;
+    gender: number;
+    marital_status: number;
+    home_address: string;
+    phone: string;
+    department: number;
+    education: number;
+    education_field: number;
+    job_level: number;
+    job_role: number;
+    number_of_companies_worked: number;
+    monthly_salary: number;
+    percent_salary_hike: number;
+    company_id: number;
+    user_id?: number;
+}
