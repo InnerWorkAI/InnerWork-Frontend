a8ea748db0107db141b466e4e710ea7622453acd
AntonioDR01 <antoniodr19903@gmail.com>
2026-03-01 21:02:01 +0100

    feat: empoyee-chart-componet terminado, logica de la grafica de satisfacción del admin-dashboard implementada

--- src/app/shared/components/employee-chart/employee-chart.component.ts
+++ src/app/shared/components/employee-chart/employee-chart.component.ts
@@ -11,6 +11,12 @@ import {
   ApexFill 
 } from 'ng-apexcharts';
 
+export interface ChartLegendItem {
+  label: string;
+  color: string;
+  isLine?: boolean;
+}
+
 @Component({
   selector: 'app-employee-chart',
   standalone: true,
@@ -18,45 +24,42 @@ import {
   templateUrl: './employee-chart.component.html',
   styleUrls: ['./employee-chart.component.scss']
 })
+
 export class EmployeeChartComponent{
   public title = input<string>('');
   public subtitle = input<string>('');
   public legend = input<ChartLegendItem[]>([]);
-  
+  public chartColors = input<string[]>(['#a855f7']);  
   public series = input<ApexAxisChartSeries>([]); 
   public categories = input<string[]>([]);
 
-  private primaryColor = getComputedStyle(document.documentElement).getPropertyValue('--ion-color-primary').trim() || '#9333ea';
-  private redColor = '#aa0b0b';
+  public chart: ApexChart = {
+    type: 'line', 
+    height: 350, 
+    toolbar: { show: false }, 
+    animations: { enabled: true/* , easing: 'easeinout', speed: 800 */ } 
+  };
+  public colors = computed(() => this.chartColors());
+  public stroke: ApexStroke = { curve: 'smooth', width: 2.5 };
+  
+  public yaxis: ApexYAxis = { 
+    min: 0, 
+    max: 100, 
+    tickAmount: 5, 
+    labels: { formatter: (val) => val.toFixed(0) + "%", style: { colors: '#a4a4a4' } } 
+  };
 
-  public chart: ApexChart = { type: 'line', height: 350, toolbar: { show: false }, animations: { enabled: true } };
-  public colors = [this.primaryColor];
-  public stroke: ApexStroke = { curve: 'smooth', width: 3 };
-  public yaxis: ApexYAxis = { min: 0, max: 100, tickAmount: 5, labels: { formatter: (val) => val.toFixed(0) + "%", style: { colors: '#a4a4a4' } } };
-  public tooltip: ApexTooltip = { theme: 'light', y: { formatter: (val) => val + "%" } };
+  public tooltip: ApexTooltip = { 
+    theme: 'light', 
+    y: { formatter: (val) => val + "%" } };
 
   public xaxis = computed<ApexXAxis>(() => ({
-    categories: this.categories(),
+    categories: this.categories(),    
     labels: { style: { colors: '#a4a4a4' } }
   }));
 
   public fill: ApexFill = {
-    type: 'gradient',
-    gradient: {
-      type: 'vertical',
-      shadeIntensity: 0.5,
-      colorStops: [
-        { offset: 0, color: this.primaryColor, opacity: 1 },
-        { offset: 60, color: this.primaryColor, opacity: 1 },
-        { offset: 80, color: this.redColor, opacity: 1 },
-        { offset: 100, color: this.redColor, opacity: 1 }
-      ]
-    }
+    type: 'solid',
+    opacity: 0.9
   };
-}
-
-export interface ChartLegendItem {
-  label: string;
-  color: string;
-  isLine?: boolean;
 }
