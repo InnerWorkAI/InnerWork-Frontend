cdafa4f30c3bfb8a6226def40eec56a26696421a
Jesushs4 <jherrerasanchez04@gmail.com>
2026-03-01 16:27:59 +0100

    deploy environment fix

--- set-env.js
+++ set-env.js
@@ -1,20 +1,27 @@
 const fs = require('fs');
+const path = require('path');
+
+const envDirectory = './src/environments';
+
+if (!fs.existsSync(envDirectory)) {
+  fs.mkdirSync(envDirectory, { recursive: true });
+  console.log('📁 Carpeta src/environments creada porque no existía');
+}
 
 const paths = [
   './src/environments/environment.ts',
   './src/environments/environment.prod.ts'
 ];
 
-const envConfigFile = `
-export const environment = {
+const envConfigFile = `export const environment = {
   production: true,
-  locationIqToken: '${process.env.LOCATION_IQ_TOKEN || ""}',
-  API_URL: '${process.env.API_URL || ""}',
-  groqKey: '${process.env.GROQ_KEY || ""}'
-};
-`;
+  locationIqToken: '${(process.env.LOCATION_IQ_TOKEN || "").trim()}',
+  apiUrl: '${(process.env.API_URL || "").trim()}',
+  groqKey: '${(process.env.GROQ_KEY || "").trim()}'
+};`;
 
-paths.forEach(path => {
-  fs.writeFileSync(path, envConfigFile);
-  console.log(`✅ Generado: ${path}`);
+// 2. Ahora sí, escribimos los archivos
+paths.forEach(filePath => {
+  fs.writeFileSync(filePath, envConfigFile);
+  console.log(`✅ Archivo generado: ${filePath}`);
 });
