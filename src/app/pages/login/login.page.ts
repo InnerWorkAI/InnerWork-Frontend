124a6ba4f2aea852f12f32c02335c191eeffd34e
Jesushs4 <jherrerasanchez04@gmail.com>
2026-03-01 01:47:40 +0100

    fix: test environment variables in vercel

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -5,6 +5,7 @@ import { IonContent, IonHeader, IonTitle, IonToolbar, IonText, IonInput, IonIcon
 import { LoginFormComponent } from 'src/app/shared/components/login-form/login-form.component';
 import { AuthService } from 'src/app/core/services/auth-service';
 import { LoginCredentials } from 'src/app/shared/models/User';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-login',
@@ -26,6 +27,11 @@ export class LoginPage  {
       next: (res) => console.log('Éxito:', res),
       error: (err) => {
         const message = err.error?.detail || 'Correo o contraseña incorrectos';
+        if (environment.apiUrl) {
+          console.log("HAY URL DE API")
+        } else {
+          console.log("NO HAY URL DE API")
+        }
         this.errorMessage.set(message);
         console.log(message)
       }
